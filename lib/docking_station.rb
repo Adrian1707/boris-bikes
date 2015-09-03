fef554315cdbdf15600958e68a8d03e01094310b
Adrian Booth <Adrian.booth17@gmail.com>
2015-09-03 22:32:35 +0100

    working Thursday night

---
+++ lib/docking_station.rb
@@ -0,0 +1,16 @@
+require_relative 'Bike'
+class DockingStation
+	def initialize 
+		@bikes = []
+	end 
+
+	def release_bike
+		fail "There are no bikes" if @bikes.empty?
+		@bikes.pop
+	end 
+
+	def dock(bike)
+		fail "Docking station is full" if @bikes.count >= 20
+		@bikes << bike 
+	end 
+end 
---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,26 @@
+require 'docking_station'
+
+describe DockingStation  do 
+	
+	describe '#release_bike' do 
+		it "Raise error when station is empty" do 
+		expect { subject.release_bike }.to raise_error("There are no bikes")
+	end  
+	end 
+
+	describe '#dock' do 
+		it 'Raise error when full' do 
+		20.times {subject.dock Bike.new}
+		expect { subject.dock Bike.new }.to raise_error("Docking station is full")
+	end 
+	end 
+
+	it { is_expected.to respond_to :release_bike }
+	it { is_expected.to respond_to(:dock).with(1).argument }
+	it 'releases working bikes' do 
+		subject.dock Bike.new 
+		bike = subject.release_bike
+		expect(bike).to be_working
+	end
+	
+end
